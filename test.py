70878eeef52ac4dd8a3e117a1fe0237873283f02
Li XI <lieinsteinxi@gmail.com>
2018-08-21 13:29:10 +0800

    First successful communication with controller

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import serial
-ser = serial.Serial('/dev/uart.BLTH', 115200)
+ser = serial.Serial('/dev/serial/by-id/usb-Thorlabs_Stepper_Controller_26001411-if00-port0', 115200)
 des = 0x50  # generic USB
 source = 0x01  # host
 cmdIdentify = bytearray([ 0x23, 0x02, 0, 0, des, source ])
