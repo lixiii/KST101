6e850d02abbf7fec4d8e43c30c0e5fa5d0296966
Li XI <lieinsteinxi@gmail.com>
2018-08-21 13:37:54 +0800

    added helpful colors

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import serial
+from bcolours import BC
+
 ser = serial.Serial('/dev/serial/by-id/usb-Thorlabs_Stepper_Controller_26001411-if00-port0', 115200)
 des = 0x50  # generic USB
 source = 0x01  # host
 cmdIdentify = bytearray([ 0x23, 0x02, 0, 0, des, source ])
+print(BC.OKGREEN + "Sending command 'MGMSG_MOD_IDENTIFY' to controller. " + BC.ENDC)
 ser.write(cmdIdentify)
 ser.close()
